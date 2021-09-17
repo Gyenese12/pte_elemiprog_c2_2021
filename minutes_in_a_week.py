029808a9c3ccc269d0f4b6a544ebba0cd7f5990b
Gyenese12 <gyenese94@gmail.com>
2021-09-17 15:26:58 +0200

    a

---
+++ minutes_in_a_week.py
@@ -0,0 +1,12 @@
+day = 7
+hour = 24
+min = 60
+print(day*hour*min)
+hour = 26
+print(day*hour*min)
+
+
+lada = 124
+osztaly = 34
+ossz = lada//osztaly
+print("mindenkinek", ossz,"db jut. A tanárnőnél: ",lada-ossz*osztaly,"marad")
--- variable.py
+++ variable.py
@@ -2,10 +2,11 @@ kiscica = 5
 print (kiscica + 5)
 print(kiscica**3)
 
-width = 4
-length = 5
+width,length = 4,5
 area = width * length
 print("a szoba területe",area,"m^2")
 length2 = 2;
 area = width*length2
-print ("a szoba területe: ",area,"m^2")
+print ("a szoba területe:",area,"m^2")
+width3 = length3 = 5
+print(width3*length3)
