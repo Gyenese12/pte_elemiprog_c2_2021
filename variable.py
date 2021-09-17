094c60dadf13bef108a07eb1231bdff33f4b6813
Gyenese12 <gyenese94@gmail.com>
2021-09-17 15:10:00 +0200

    a

--- variable.py
+++ variable.py
@@ -1,2 +1,11 @@
 kiscica = 5
 print (kiscica + 5)
+print(kiscica**3)
+
+width = 4
+length = 5
+area = width * length
+print("a szoba területe",area,"m^2")
+length2 = 2;
+area = width*length2
+print ("a szoba területe: ",area,"m^2")
